5028929476f598a84eff86c2ff42910b8f2196f7
luongtheluc <ltl.luongtheluc@gmail.com>
2023-05-18 10:40:46 +0700

    Authorization role base

--- HueOnlineTicketFestival/Program.cs
+++ HueOnlineTicketFestival/Program.cs
@@ -3,6 +3,8 @@ using HueOnlineTicketFestival.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddAuthentication().AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            builder.Configuration.GetSection("AppSettings:SecretKey").Value!))
+    };
+});
+
 builder.Services.AddDbContext<FestivalTicketContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -30,7 +54,6 @@ builder.Services.AddScoped<IRoleService, RoleService>();
 builder.Services.AddScoped<ITicketCheckinService, TicketCheckinService>();
 builder.Services.AddScoped<ITicketTypeService, TicketTypeService>();
 
-
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
@@ -39,6 +62,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
